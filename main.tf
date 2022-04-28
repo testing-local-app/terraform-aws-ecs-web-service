a9c3f1c1af9e069e4f79505b2fcb452c852d1bb1
badars1 <97477032+badars1@users.noreply.github.com>
2022-04-28 16:45:31 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -149,3 +149,88 @@ resource "aws_appautoscaling_policy" "down" {
     "aws_appautoscaling_target.main",
   ]
 }
+
+resource "aws_security_group_rule" "app_lb_https_ingress" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = "${module.app_web_service.lb_security_group_id}"
+}
+
+resource "aws_ecs_task_definition" "app" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  family                = "ProductionApp"
+  container_definitions = "..."
+}
+
+module "app_web_service" {
+  source = "github.com/azavea/terraform-aws-ecs-web-service?ref=0.4.0"
+
+  name                = "App"
+  vpc_id              = "vpc-..."
+  public_subnet_ids   = ["subnet-...", "subnet-..."]
+  access_log_bucket   = "logs-bucket"
+  access_log_prefix   = "ALB"
+  health_check_path   = "/health-check/"
+  port                = "8080"
+  ssl_certificate_arn = "arn..."
+
+  cluster_name                   = "default"
+  task_definition_id             = "${aws_ecs_task_definition.app.family}:${aws_ecs_task_definition.app.revision}"
+  desired_count                  = "1"
+  min_count                      = "1"
+  max_count                      = "2"
+  scale_up_cooldown_seconds      = "300"
+  scale_down_cooldown_seconds    = "300"
+  deployment_min_healthy_percent = "100"
+  deployment_max_percent         = "200"
+  container_name                 = "django"
+  container_port                 = "8080"
+  
+  ecs_service_role_name = "..."
+
+  project     = "${var.project}"
+  environment = "${var.environment}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "app_service_high_cpu" {
+  alarm_name          = "alarmAppCPUUtilizationHigh"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "60"
+
+  dimensions {
+    ClusterName = "default"
+    ServiceName = "App"
+  }
+
+  alarm_actions = ["${module.app_web_service.appautoscaling_policy_scale_up_arn}"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "app_service_low_cpu" {
+  alarm_name          = "alarmAppCPUUtilizationLow"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "15"
+
+  dimensions {
+    ClusterName = "default"
+    ServiceName = "App"
+  }
+
+  alarm_actions = ["${module.app_web_service.appautoscaling_policy_scale_down_arn}"]
+}
